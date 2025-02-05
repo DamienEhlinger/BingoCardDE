13de12cc8de8c94d4b0f3f4039e9e906ac588204
DamienEhlinger <ehlingerd4685@my.uwstout.edu>
2025-02-05 00:47:44 -0600

    added some initial things to the Mech and Reader classes.

--- BingoMech.java
+++ BingoMech.java
@@ -1,3 +1,7 @@
 public class BingoMech {
     /*This will be where the mechanics of the this game can be built and called in Main when needed */
+
+    public BingoMech() {
+        /*This will be the constructor for the BingoMech class */
+    }
 }
--- BingoReader.java
+++ BingoReader.java
@@ -1,4 +1,20 @@
 public class BingoReader {
     // This will be where the reading of the bingo card will be done
+    private String bingoCardFile;
+    private String[][] bingoCardArray;
 
+    //This will be the constructor for the BingoReader class
+    public BingoReader() {
+        //This will be the constructor for the BingoReader class
+    }
+
+    //This will be the method to read the bingo card
+    public void readBingoCard() {
+        //This will be the method to read the bingo card
+    }
+    
+    //This will be the method to create the 2D array to store the bingo card
+    public void createBingoCardArray() {
+        //This will be the method to create the 2D array to store the bingo card
+    }
 }
